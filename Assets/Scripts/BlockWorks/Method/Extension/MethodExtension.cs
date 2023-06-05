f17e2da5cc9549084cbecae342b467b87b0ab10e
BlockedByRain <1596127831@qq.com>
2023-06-06 01:26:42 +0800

    版本从 2020.3.15f2c1 升级到 2021.3.26f1c1
    初步实现圆形菜单，后续需优化

--- Assets/Scripts/BlockWorks/Method/Extension/MethodExtension.cs
+++ Assets/Scripts/BlockWorks/Method/Extension/MethodExtension.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public static class MethodExtension 
 {
 
-    static public T GetOrAddComponent<T>(this GameObject go) where T : Component
+    public static T GetOrAddComponent<T>(this GameObject go) where T : Component
     {
         T ret = go.GetComponent<T>();
         if (null == ret)
             ret = go.AddComponent<T>();
         return ret;
+    }
+
+    public static float GetAnlgeFromPoint(this Vector2 point, Vector2 origin)
+    {
+        Vector2 dir = new Vector2(point.x - origin.x, point.y - origin.y).normalized;
+        float angle = Mathf.Acos(dir.y) * Mathf.Rad2Deg;
+        return point.x > origin.x ? angle : 360.0f - angle;
     }
 
 }
---
+++ Assets/Scripts/CircularMenu.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+
+
+public class CircularMenu : MonoBehaviour, IPointerClickHandler, IPointerMoveHandler
+{ 
+    public KeyCode keyAction;
+
+    [SerializeField]
+    private CanvasGroup cg;
+
+    [SerializeField]
+    private Image _0;
+
+    [SerializeField]
+    private Image _1;
+
+    [SerializeField]
+    private Image _2;
+
+    [SerializeField]
+    private Image _3;
+
+    [SerializeField]
+    private Image _4;
+
+    [SerializeField]
+    private Image _5;
+
+    [SerializeField]
+    private Image _6;
+
+
+    private Color hide = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+    private Color show = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+    private bool isShow = false;
+
+    private int currentPart = 0;
+
+    public Action<int> OnClick;
+
+
+    private void Start()
+    {
+        ResetCanvas();
+        OnClick += (part) =>
+        {
+            Debug.Log(part);
+        };
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyAction))
+        {
+            Show();
+        }
+    }
+
+    private void ResetCanvas()
+    {
+        cg.alpha = 0.0f;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+        ResetColor();
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void ShowCanvas()
+    {
+        cg.alpha = 1.0f;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+        ResetColor();
+        //Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void ResetColor()
+    {
+        _0.color = hide;
+        _1.color = hide;
+        _2.color = hide;
+        _3.color = hide;
+        _4.color = hide;
+        _5.color = hide;
+        _6.color = hide;
+    }
+
+
+    private void Show()
+    {
+        isShow = !isShow;
+        if (isShow)
+        {
+            ShowCanvas();
+        }
+        else
+        {
+            ResetCanvas();
+        }
+
+    }
+
+
+    public void OnPointerMove(PointerEventData e)
+    {
+        int part = RectTransformUtility.RectangleContainsScreenPoint(_0.rectTransform, e.position) ? 0 : ((int)e.position.GetAnlgeFromPoint(new Vector2(Screen.width / 2, Screen.height / 2)) / 60) + 1;
+        if (currentPart != part)
+        {
+            currentPart = part;
+            ResetColor();
+            switch (currentPart)
+            {
+                case 0: _0.color = show; return;
+                case 1: _1.color = show; return;
+                case 2: _2.color = show; return;
+                case 3: _3.color = show; return;
+                case 4: _4.color = show; return;
+                case 5: _5.color = show; return;
+                case 6: _6.color = show; return;
+                default: _0.color = show; return;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        isShow = false;
+        ResetCanvas();
+        OnClick?.Invoke(currentPart);
+    }
+
+    
+
+    
+}
